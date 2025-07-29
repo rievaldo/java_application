0fe7cebe87260c8cef54ce8ed11fb515e98984ca
rievaldo <hayyernayyeng22@gmail.com>
2025-07-29 17:31:34 +0700

    second commit

---
+++ app/src/main/java/com/example/e_cuan1/UploadSuccess.java
@@ -0,0 +1,13 @@
+package com.example.e_cuan1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UploadSuccess extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.upload);
+    }
+}
